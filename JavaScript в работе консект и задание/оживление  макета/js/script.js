99f65f78126e019cb8fb81a609fe818512d569ec
Aliaksandr <aaskaldovic@gmail.com>
2020-12-10 22:46:01 +0300

    add lesson 43 window

--- JavaScript в работе консект и задание/оживление  макета/js/script.js
+++ JavaScript в работе консект и задание/оживление  макета/js/script.js
@@ -92,7 +92,47 @@ const tabs=document.querySelectorAll('.tabheader__item'),
           }
 
           setClock('.timer', deadLine);
+
+          // Modal
+          const modalTrigger = document.querySelectorAll('[data-modal]'),
+                modal =document.querySelector('.modal'),
+                modalCloseBtn = document.querySelector('[data-close');
+
+                console.log(modalCloseBtn);
+
+          modalTrigger.forEach(item =>{
+            item.addEventListener('click', () =>{
+              modal.classList.toggle('show');
+              // modal.classList.add('show');
+              // modal.classList.remove('hide');
+              document.body.style.overflow = 'hidden';
+            });
+          });
+          function closeModal(){
+            modal.classList.toggle('show');
+            // modal.classList.add('hide');
+            // modal.classList.remove('show');
+            document.body.style.overflow = '';
+          }
+          modalCloseBtn.addEventListener('click', closeModal);
+            
+
+          modal.addEventListener('click', (e) =>{
+            if(e.target === modal){
+             closeModal();
+            }
+          });
+
+          document.addEventListener('keydown', (e) =>{
+            if(e.code =='Escape' && modal.classList.contains('show')){
+              closeModal();
+            }
+          });
+
+
 });
+
+
 
 
 
