274a706fb684c67e29d874f5667908c29e9e4d45
Alex <aaskaldovic@gmail.com>
2021-01-07 20:57:41 +0300

    add async/await

--- продвинутый JavaScript конспект и задания/food/js/script.js
+++ продвинутый JavaScript конспект и задания/food/js/script.js
@@ -196,45 +196,45 @@ const tabs=document.querySelectorAll('.tabheader__item'),
 
           }
 
+          const getResource = async (url)=>{
+            const res = await fetch(url);
 
-          new MenuCard(
-            "img/tabs/vegy.jpg",
-            "vegy",
-            'Меню "Фитнес"',
-            `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.
-             Продукт активных и здоровых людей.
-             Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
-            9,
-            '.menu .container',
-            //'menu__item', /* передаем без точки так как эти данные потом запишутся в массив */
-            'big'
-          ).render(); // короткий способ создания элземпляров класса
+            if(!res.ok){
+             throw new Error(`Could not fetch ${url}, status: ${res.status}`); //объект ошибки
+            }
+            return await res.json();//это Promies
+          };
 
-          new MenuCard(
-            "img/tabs/elite.jpg",
-            "elite",
-            'Меню “Премиум”',
-            `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. 
-            Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
-            11,
-            '.menu .container'
-            //'menu__item',
-            //'big'
-          ).render();
+          getResource('http://localhost:3000/menu')
+              .then(data=>{
+                data.forEach(({img, altimg, title, descr, price}) =>{
+                  new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+                });
+              });
 
-          new MenuCard(
-            "img/tabs/post.jpg",
-            "post",
-            'Меню "Постное"',
-            `Меню “Постное” - это тщательный подбор ингредиентов:
-             полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, 
-             правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
-            14,
-            '.menu .container'
-            //'menu__item',
-            //'big'
-          ).render();
+          // getResource('http://localhost:3000/menu')
+          // .then(data =>createCard(data));
 
+          // function createCard(data){
+          //   data.forEach(({img, altimg, title, descr, price})=>{
+          //       const element = document.createElement('div');
+
+          //       element.classList.add('menu__item');
+
+          //       element.innerHTML =  `
+          //     <img src=${img} alt=${altimg}>
+          //     <h3 class="menu__item-subtitle">${title}</h3>
+          //     <div class="menu__item-descr">${descr}</div>
+          //     <div class="menu__item-divider"></div>
+          //     <div class="menu__item-price">
+          //         <div class="menu__item-cost">Цена:</div>
+          //         <div class="menu__item-total"><span>${price}</span> грн/день</div>
+          //     </div>`;
+          //     document.querySelector('.menu .container').append(element);
+          //   });
+          // }
+
+ 
         //lesson 53 Реализация скрипта для отправки данных на сервер
 
          // Forms
@@ -247,10 +247,23 @@ const tabs=document.querySelectorAll('.tabheader__item'),
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data)=>{
+      const res = await fetch(url, {
+        method: "POST",
+              headers:  {
+                'content-type': 'application/json'
+              },
+              body: data
+      });
+      return await res.json();//это Promies
+    };
+
+
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -265,24 +278,9 @@ const tabs=document.querySelectorAll('.tabheader__item'),
            form.insertAdjacentElement('afterend', statusMessage);//после конца формы
             
            const formData = new FormData(form);//получение данных с формы
-
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
+           const json =JSON.stringify(Object.fromEntries(formData.entries()));//перевод данных с формы в json
            
-           
-
-            fetch('server.php',{//если Promise попадает на ошибку связанную с http проттоколом то он НЕ выкинет reject
-              // он норм выполнит resolve, reject будет возникать при сбое сети или если что-то помешало запросу вып-ся
-              //(offline)
-              method: "POST",
-              headers:  {
-                'content-type': 'application/json'
-              },
-              body: JSON.stringify(object)
-            })
-            .then(data => data.text())//модификация ответа, чтобы точно понимать что приходит от сервера
+            postData('http://localhost:3000/requests', json)
             .then(data =>{
                   console.log(data);
                   showThanksModal(message.success);
@@ -321,9 +319,9 @@ const tabs=document.querySelectorAll('.tabheader__item'),
       }, 4000);
     }
 
-    fetch('  http://localhost:3000/menu')
-        .then(data=>data.json())
-        .then(res =>console.log(res));
+    // fetch('  http://localhost:3000/menu')
+    //     .then(data=>data.json())
+    //     .then(res =>console.log(res));
 
 
     // lesson 56 Fetch API
