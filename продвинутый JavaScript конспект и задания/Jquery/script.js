9f8d8db8bbc4db011ba33621d9d5153f000ede3b
Alex <aaskaldovic@gmail.com>
2021-01-30 20:50:52 +0300

    add jquery

---
+++ продвинутый JavaScript конспект и задания/Jquery/script.js
@@ -0,0 +1,23 @@
+// import $ from 'jquery'
+// import  'jquery'
+
+console.log($('#btn:first'));
+console.log($('.list-item:first'));
+
+$(document).ready(function(){
+    $('.list-item:first').hover(function(){
+       
+        $(this).toggleClass('active');
+       
+    });
+
+    $('.list-item').eq(4).on('click', function(){
+        console.log('aassf');
+        $('.image:odd').animate({
+            opacity: 'toggle',
+            height: 'toggle' 
+        }, 2000);
+    });     
+
+    
+});
